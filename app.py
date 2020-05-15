9bb1e1ae0b1c86e40d58f017a41c8616953557fa
rafael.ferreira <rafah.26ferreira@hotmail.com>
2020-05-15 18:14:18 -0300

    converting response to JSON

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def home():
-    return "Server Works!"
+    return jsonify({"Status":"Server Works!"})
 
 app.run(debug=True)
