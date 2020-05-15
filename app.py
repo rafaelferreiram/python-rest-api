4eb5bd2c1bc0b49205e0431521c218a1058b8fd8
rafael.ferreira <rafah.26ferreira@hotmail.com>
2020-05-15 18:45:12 -0300

    communicating with mongoDB remotly

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
 from flask import Flask, jsonify
+from flask_pymongo import PyMongo
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb+srv://admin:pwdadmin@dynamic-dug18.mongodb.net/py-db?retryWrites=true&w=majority"
+app.config["MONGO_DBNAME"] = "python"
+mongo = PyMongo(app)
 
 @app.route("/", methods=["GET"])
 def home():
     return jsonify({"Status":"Server Works!"})
+
+@app.route("/insert", methods=["GET"])
+def add_star():
+  pythonDb = mongo.db.python
+  name = "Rafael"
+  distance = "1km"
+  star_id = pythonDb.insert({'name': name, 'distance': distance})
+  new_star = pythonDb.find_one({'_id': star_id })
+  output = {'name' : new_star['name'], 'distance' : new_star['distance']}
+  return jsonify({'result' : output})
 
 app.run(debug=True)
